a7293df0d347943568a375815008eb48ed873596
George Hilliard <gh403@msstate.edu>
2013-11-11 15:36:27 -0600

    Reorganize TI bit-banging code into tilink.h and tilink.c

---
+++ sidekick.X/tilink.c
@@ -0,0 +1,116 @@
+/*
+ * File:   tilink.c
+ * Author: George Hilliard
+ *
+ * Created on November 11, 2013, 3:27 PM
+ */
+
+#include <stdio.h>
+#include "pic24_all.h"
+
+#include "tilink.h"
+
+typedef enum {
+    floating = 0,
+    ring,
+    tip
+} pin;
+
+#define TIPPIN _RB5
+#define TIPLATCH _LATB5
+#define CONFIG_TIP_AS_OUTPUT() CONFIG_RB5_AS_DIG_OUTPUT()
+#define CONFIG_TIP_AS_INPUT()  CONFIG_RB5_AS_DIG_INPUT()
+
+#define RINGPIN _RB6
+#define RINGLATCH _LATB6
+#define CONFIG_RING_AS_OUTPUT() CONFIG_RB6_AS_DIG_OUTPUT()
+#define CONFIG_RING_AS_INPUT()  CONFIG_RB6_AS_DIG_INPUT()
+
+struct {
+    unsigned char *front;
+    unsigned char *back;
+    unsigned char data[256];
+    unsigned char bits;
+    unsigned char shiftbyte;
+} TIfifo;
+
+void TIfifo_addBit(unsigned char newbit)
+{
+    // No overflow detection is performed!
+    TIfifo.shiftbyte = (TIfifo.shiftbyte << 1) | newbit;
+    if((TIfifo.bits += 1) == 8) {
+        *TIfifo.back = TIfifo.shiftbyte;
+        if(++TIfifo.back == TIfifo.data + sizeof(TIfifo.data))
+            TIfifo.back = &TIfifo.data;
+        TIfifo.bits = 0;
+    }
+}
+
+void _ISRFAST _CNInterrupt(void) {
+    static pin state = floating;
+
+    if(TIPPIN == 0 && state == floating) {
+        CONFIG_RING_AS_OUTPUT();
+        RINGLATCH = 0;
+        state = tip;
+        TIfifo_addBit(0);
+#ifndef NDEBUG
+        putchar('0');
+#endif
+    }
+    else if(RINGPIN == 0 && state == floating) {
+        CONFIG_TIP_AS_OUTPUT();
+        TIPLATCH = 0;
+        state = ring;
+        TIfifo_addBit(1);
+#ifndef NDEBUG
+        putchar('1');
+#endif
+    }
+    else if(state == tip && TIPPIN) {
+        state = floating;
+        RINGLATCH = 1;
+        CONFIG_RING_AS_INPUT();
+    }
+    else if(state == ring && RINGPIN) {
+        state = floating;
+        TIPPIN = 1;
+        CONFIG_TIP_AS_INPUT();
+    }
+    else {
+        // No clue what happened.
+        putchar('X');
+        CONFIG_RING_AS_OUTPUT();
+        CONFIG_TIP_AS_OUTPUT();
+        // Signal an error
+        RINGLATCH = 0;
+        TIPLATCH = 0;
+        DELAY_US(500);
+
+        asm("RESET");
+    };
+
+    _CNIF = 0;
+}
+
+void configTIlink()
+{
+    // Configure tip
+    CONFIG_TIP_AS_INPUT();
+    DISABLE_RB5_PULLUP();
+    ENABLE_RB5_OPENDRAIN();
+    ENABLE_RB5_CN_INTERRUPT();
+
+    // Configure ring
+    CONFIG_RING_AS_INPUT();
+    DISABLE_RB6_PULLUP();
+    ENABLE_RB6_OPENDRAIN();
+    ENABLE_RB6_CN_INTERRUPT();
+
+    TIPLATCH = 1;
+    RINGLATCH = 1;
+
+    _CNIF = 0;                       //clear interrupt flag
+    _CNIP = 1;                       //choose a priority
+    _CNIE = 1;                       //enable the interrupt
+}
---
+++ sidekick.X/tilink.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   tilink.h
+ * Author: George Hilliard
+ *
+ * Created on November 11, 2013, 3:28 PM
+ */
+
+#ifndef TILINK_H
+#define	TILINK_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void configTIlink();
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TILINK_H */
+
