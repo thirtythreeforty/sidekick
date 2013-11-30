d4e5c3e2b832249b7783ab3b3e6404aa36ca16ff
George Hilliard <gh403@msstate.edu>
2013-11-29 23:21:13 -0600

    Implement variable send
    The variable send code is able to completely handle a transaction
    with the TI.  The only thing that remains after this commit is code
    cleanup and the correct reset-wrangling of the EEPROMs.

--- sidekick.X/variable.c
+++ sidekick.X/variable.c
@@ -8,6 +8,7 @@
 #include "pic24_all.h"
 #include "common.h"
 #include "tipacket.h"
+#include "tilink.h"
 #include "eeprom.h"
 #include "variable.h"
 
@@ -91,6 +92,74 @@ void  _ISRFAST _T2Interrupt(void)
     _T2IF = 0;
 }
 
+void _ISRFAST _INT1Interrupt(void)
+{
+    eepromHeader erHeader;
+    unsigned long int i;
+
+    debug("Sending variables to TI.\n");
+
+    // Send everything, one at a time.
+    while(eepromStart(read, 0x000000))
+        ;//asm(" pwrsav #1");
+    eepromReadArray(&erHeader, sizeof(erHeader));
+    eepromStop();
+
+    debug("%i vars to send.\n", erHeader.numVariables);
+
+    if(erHeader.numVariables) {
+        for(i = EEPROM_PAGE_SIZE; i < EEPROM_PAGE_SIZE * (erHeader.numVariables + 1);) {
+            unsigned int n, checksum = 0;
+            TIvarHeader varHeader;
+            while(eepromStart(read, i))
+                ;//asm(" pwrsav #1");
+            eepromReadArray(&varHeader, sizeof(varHeader));
+            // constant header size + name's size + extra byte
+            sendTIPacket(erHeader.calcType, 0x06, (unsigned char*)&varHeader, 6 + varHeader.varNameSize + 1);
+
+            // Get the CTS
+            TIfifo_getByte();
+            if(TIfifo_getByte() != 0x09)
+                error_and_reset();
+            TIfifo_getByte();
+            TIfifo_getByte();
+
+            sendTIAck(0x98, ACK);
+
+            debug("Sending variable...\n");
+            // Ok, send the data, plus 4 extra bytes (?!).  Have to do this manually.
+            setTIlinkMode(send);
+            TIfifo_addByte(erHeader.calcType);
+            TIfifo_addByte(0x15);
+            TIfifo_addByte((varHeader.dataSize + 4) & 0xFF);
+            TIfifo_addByte((varHeader.dataSize + 4) >> 8);
+            for(n = 0; n < varHeader.dataSize + 4; ++n) {
+                unsigned char b = eepromReadByte();
+                checksum += b;
+                TIfifo_addByte(b);
+            }
+            TIfifo_addByte(checksum & 0xFF);
+            TIfifo_addByte(checksum >> 8);
+            setTIlinkMode(receive);
+
+            eepromStop();
+
+            // Update i
+            i += sizeof(varHeader) + varHeader.dataSize;
+            unsigned int remainder = i % EEPROM_PAGE_SIZE;
+            if (remainder != 0)
+                i += EEPROM_PAGE_SIZE - remainder;
+        }
+
+        // Now, send an EOT
+        debug("Sending EOT...\n");
+        sendTIPacket(erHeader.calcType, 0x92, 0, 0);
+    }
+
+    DELAY_MS(DEBOUNCE_DLY); // Switch bounce
+    _INT1IF = 0;
+}
+
 void configVariable(void)
 {
     eepromReset();
@@ -104,9 +173,18 @@ void configVariable(void)
     PR2 = usToU16Ticks(250, getTimerPrescale(T2CONbits)) - 1;
     TMR2  = 0;
     _T2IF = 0;
-    _T2IP = 1;
+    _T2IP = 2;
     _T2IE = 0; // Let variableVerifyAndInit() enable the interrupt.
     T2CONbits.TON = 1;
+
+    CONFIG_RB3_AS_DIG_INPUT();
+    ENABLE_RB3_PULLUP();
+    DELAY_US(1); // wait for pullup
+    CONFIG_INT1_TO_RP(35);
+    _INT1IP = 1;
+    _INT1EP = 1; // negative edge
+    _INT1IF = 0;
+    _INT1IE = 1;
 }
 
 void variableCommit(void)
